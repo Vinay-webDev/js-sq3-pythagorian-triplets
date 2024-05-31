42b2b6773da30bb9fa81963516ec3e89ff017533
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-31 23:04:50 +0530

    3. I'm kinda stuck at this point:((

--- js/pythagoras.js
+++ js/pythagoras.js
@@ -1,6 +1,19 @@
+// check for pythagorian triplets
 
+const a = document.getElementById("a");
+const b = document.getElementById("b");
+const c = document.getElementById("c");
 
+const hyp  = a*a;
+const opp = b*b;
+const adj = c*c;
 
+const sumOfOthe2Sides = opp + adj;
+
+function checkForTriplets() {
+    
+}
+    
 
 
 
