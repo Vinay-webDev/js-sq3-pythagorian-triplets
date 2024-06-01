b75f041bb322ec84adc68bd52b2d4fb2586f6a12
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-01 14:20:45 +0530

    4.  found out what went wrong😀😡
    now it's fully functional web app

--- js/pythagoras.js
+++ js/pythagoras.js
@@ -1,27 +1,53 @@
 // check for pythagorian triplets
+/*
+let a = document.getElementById("a").value;
+let b = document.getElementById("b").value;
+let c = document.getElementById("c").value;
+let result = document.getElementById("result");
 
-const a = document.getElementById("a");
-const b = document.getElementById("b");
-const c = document.getElementById("c");
+let hyp = a * a;
+let opp = b * b;
+let adj = c * c;
 
-const hyp  = a*a;
-const opp = b*b;
-const adj = c*c;
-
-const sumOfOthe2Sides = opp + adj;
-
-function checkForTriplets() {
-    
+function checkForTriplets () {
+    if (hyp === opp + adj) {
+        result.innerHTML = "<P id='result'> yes! </p>";
+        result.style.color = "green";
+    }
+    else {
+        result.innerHTML = "<p id='result'> no! </p>";
+        result.style.color = "red";
+    }
 }
+*/
+/* from here I used console to check what actually happening under the hood 
+and I found out the values are actually need to be entered after the click on 
+check so that we need to have those a, b, c values enetered in the web app */
+function checkForTriplets() {
+    let a = document.getElementById("a").value;
+    let b = document.getElementById("b").value;
+    let c = document.getElementById("c").value;
     
+    const result = document.getElementById("result");
 
-
-
-
-
-
-
-
+    let hyp = a * a;
+    let opp = b * b;
+    let adj = c * c;
+    
+    console.log(hyp);
+    console.log(opp);
+    console.log(adj);
+    if (hyp = opp + adj) {
+        result.textContent = "yes!";
+        result.style.color = "green";
+        console.log("true");
+    }
+    else {
+        result.textContent = "no!";
+        result.style.color = "red";
+        console.log("flase");
+    }
+}
 
 
 
